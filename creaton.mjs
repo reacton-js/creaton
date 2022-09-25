918cf122bbf05a2d628756469bb140b30273516a
reacton-js <reacton@bk.ru>
2022-09-26 01:37:13 +0300

    update

--- creaton.mjs
+++ creaton.mjs
@@ -2,30 +2,16 @@ export function event(...args) {
   (this || document).dispatchEvent(new CustomEvent(...args))
 }
 
-class Methods extends HTMLElement {
-  constructor() {
-    super()
-  }
-  $(selector) {
-    return this.$root.querySelector(selector)
-  }
-  $$(selector) {
-    return this.$root.querySelectorAll(selector)
-  }
-  $event(...args) {
-    event.apply(this, args)
-  }
-}
-
 export default (...args) => Promise.all(args.map(object => new Promise(ready => {
-  const { name, extends:extend, mode = 'open', data, render,
-    attributes, changed, before, after, connected, disconnected, adopted } = object
-
-  customElements.define(name, class extends Methods {
+  const { name, extends:extend, mode, data, render,
+    attributes, changed, before, after, connected, disconnected, adopted } = object,
+    SUPERElement = extend ? Object.getPrototypeOf(document.createElement(extend)).constructor : HTMLElement
+  
+  customElements.define(name, class extends SUPERElement {
     constructor() {
       super()
       Object.defineProperties(this, {
-        $root: { value: this.attachShadow({ mode }) },
+        $root: { value: mode ? this.attachShadow({ mode }) : this },
         $host: { value: this }
       })
     }
@@ -36,6 +22,18 @@ export default (...args) => Promise.all(args.map(object => new Promise(ready => 
       if (typeof render === 'function') this.$root.innerHTML = await render.call(this.$data || this)
       if (typeof after === 'function') await after.call(this)
       return Date.now() - time
+    }
+
+    $(selector) {
+      return this.$root.querySelector(selector)
+    }
+
+    $$(selector) {
+      return this.$root.querySelectorAll(selector)
+    }
+
+    $event(...args) {
+      event.apply(this, args)
     }
 
     static get observedAttributes() {
